febac53e0e6a12aa8449e5a4386cca95bda949dd
Yaseen <yaseenbinashraf469@gmail.com>
2026-03-29 11:29:35 +0530

    updates

--- honeypy.py
+++ honeypy.py
@@ -5,17 +5,17 @@ from ssh_honeypot import *
 
 # Parse Arguments
 
-if __name__ == "_main__":
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-a', '--address', type=str, required=True)
     parser.add_argument('-p', '--port', type=int, required=True)
     parser.add_argument('-u', '--username', type=str)
-    parser.add_argument('-pq', '--password', type=str)
+    parser.add_argument('-pw', '--password', type=str)
 
     parser.add_argument('-s', '--ssh', action="store_true")
 
-    args - parser.parse_args()
+    args = parser.parse_args()
 
     print("[-] Running SSH Honeypot")
     honeypot(args.address, args.port, args.username, args.password)
--- ssh_honeypot.py
+++ ssh_honeypot.py
@@ -172,5 +172,3 @@ def honeypot(address, port, username, password):
      
             except Exception as error:
                 print(error)
-
-honeypot('127.0.0.1', 2223, username=None, password=None)
